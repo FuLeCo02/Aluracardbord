7f5c8a0cab187a73d9105f0ce956d81255caaf35
FULECO <00001086133328sp@al.educacao.sp.gov.br>
2024-11-27 09:54:58 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Animes',
+    'O que é um shonei?',
+    'Shounei é um estilo de manga e anime focado ao publico mais jovem onde é necessario que a historia evolua.'
+)
+
+criaCartao(
+    'Jogos',
+    'Qais são os 2 significados de FPS?',
+    'Os significados são, Fist Person Shooter ou Frames Por Second'
+)
+
+criaCartao(
+    'Esport',
+    'O que é o estado de Fluxo?',
+    'O estado de Fluxo é um estado onde um jogador de esporte ou de qualquer outra coisa, entra  um momento de estase, em foco total na tarefa, perde a noção do tempo, e a realiza como algo natural. Parece algo de uma historia de fantasia, mas acontece na vida real.'
+)
+
+criaCartao(
+    'Ingles',
+    'Qaul a tradução de "He is some one invencible"?',
+    'A tradução é, "Ele é alguem invencivel"'
+)
